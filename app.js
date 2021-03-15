2f102e729167caa6d28a17cc7515cd259ae5adde
Steven <steven.simko1000@gmail.com>
2021-03-15 13:32:57 -0500

    More dependencies and init code

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 const express = require('express'); //Import the express module
 require('dotenv').config(); //Load env variables
+const mysql = require('mysql');
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_SCHEMA
+});
+
+const PORT = process.env.PORT || 3000; //Will listen on the specified port, or 3000 by default
 
 const app = express(); //Create the express app
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+
+
+server.listen(PORT, () => {
+    console.log("Server started on localhost:" + PORT)
+});
+
+
+module.exports.pool = pool; //Make the connection pool global
