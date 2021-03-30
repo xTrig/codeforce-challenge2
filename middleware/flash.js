0428cd2d2d7af500796e7a69db4a691834f4fb9f
Steven <steven.simko1000@gmail.com>
2021-03-30 15:40:29 -0500

    Forgot next()

--- middleware/flash.js
+++ middleware/flash.js
@@ -2,4 +2,5 @@ module.exports = (req, res, next) => {
     res.locals.success_msg = req.flash("success");
     res.locals.error_msg = req.flash("error");
     res.locals.user = req.user;
+    next();
 }
