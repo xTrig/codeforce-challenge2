11186e39b6a862d1d8b5343241b4fd1cc77477ad
Steven <steven.simko1000@gmail.com>
2021-04-02 11:22:47 -0500

    Added security check and chat logging

--- chat/chatroom.js
+++ chat/chatroom.js
@@ -1,17 +1,34 @@
+const server = require('../app');
+const chatUtils = require('../utils/ChatUtils');
+const userUtils = require('../utils/UserUtils');
 module.exports = (io) => {
     console.log("Setting up Socket.IO server...");
     io.use((socket, next) => {
         const username = socket.handshake.auth.username;
         const roomId = socket.handshake.auth.roomId;
+        const studentId = socket.handshake.auth.studentId;
         if(!username) {
             return next(new Error("Invalid username"));
         }
         if(!roomId) {
             return next(new Error("Invalid Room"));
         }
-        socket.username = username;
-        socket.room = roomId;
-        next();
+        if(!studentId) {
+            return next(new Error("Invalid student id"));
+        }
+
+        //Verify student ID to stop ID spoofing(ish)
+        userUtils.getUserById(server.pool, studentId, (user) => {
+            if(user) {
+                socket.username = username;
+                socket.room = roomId;
+                socket.studentId = studentId;
+                next();
+            } else {
+                return next(new Error("Student ID doesn't exist"));
+            }
+        });
+        
     });
 
     io.on("connection", (socket) => { //Called when a user connects
@@ -36,7 +53,7 @@ module.exports = (io) => {
             let msgToSend = socket.username + ": " + message;
             console.log("[" + socket.room + "] " + msgToSend);
             socket.to(socket.room).emit("chat message", msgToSend);
-            
+            chatUtils.logMessage(server.pool, socket.studentId, socket.room, message);
             
         });
     });
