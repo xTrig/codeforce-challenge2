4e7caa72a3eeab4726ffdf96b933646acac5ee87
Steven <steven.simko1000@gmail.com>
2021-04-02 11:45:52 -0500

    Fetch previous messages from chatlogs

--- utils/ChatUtils.js
+++ utils/ChatUtils.js
@@ -5,3 +5,24 @@ module.exports.logMessage = (pool, userId, roomId, message) => {
         //We don't really need to do anything else here at the moment
     });
 }
+
+module.exports.getPreviousMessages = (pool, roomId, amount, callback) => {
+    let sql = "SELECT c.message, c.time, c.studentId, u.firstName, u.lastName FROM chatlogs c INNER JOIN users u ON c.studentId = u.id WHERE c.sectionId=? ORDER BY messageId DESC LIMIT ?";
+    pool.query(sql, [roomId, amount], (err, results) => {
+        if(err) throw err;
+        if(results.length > 0) {
+            let messages = [];
+            for(let i = 0; i < results.length; i++) {
+                messages.push({
+                    message: results[i].message,
+                    name: results[i].firstName + " " + results[i].lastName,
+                    time: results[i].time,
+                    studentId: results[i].studentId
+                })
+            }
+            callback(messages);
+        } else {
+            callback(null);
+        }
+    });
+}
